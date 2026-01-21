d5caae345570a4b2e1a6daf60563873cd0fd285c
rudhravyas <91895312+rudhravyas@users.noreply.github.com>
2026-01-21 23:26:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // Rectangle class
 class Rectangle {
   constructor(width, height) {
-    this._width = width;
-    this._height = height;
+    this.width = width;
+    this.height = height;
   }
 
   // Getter for width
   get width() {
-    return this._width;
+    return this.width;
   }
 
   // Getter for height
   get height() {
-    return this._height;
+    return this.height;
   }
 
   // Method to calculate area
   getArea() {
-    return this._width * this._height;
+    return this.width * this.height;
   }
 }
 
