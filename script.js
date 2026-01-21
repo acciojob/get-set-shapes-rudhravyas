fb044a89645488d8531f4d8a3092cff2d4d314ad
rudhravyas <91895312+rudhravyas@users.noreply.github.com>
2026-01-21 23:23:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-//complete this code
-class Rectangle {}
+// Rectangle class
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+// Square class (inherits from Rectangle)
+class Square extends Rectangle {
+  constructor(side) {
+    // Call Rectangle constructor with same value for width and height
+    super(side, side);
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return this.width * 4;
+  }
+}
+
+// Expose classes to Cypress
 window.Rectangle = Rectangle;
 window.Square = Square;
